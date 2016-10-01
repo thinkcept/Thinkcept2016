7882a3007e522171b01c19c8edb8bfd5b4fd3d72
thinkcept <ashismo@gmail.com>
2016-10-01 12:52:32 +0530

    added code for build and deploy

--- Thinkcept/src/main/java/com/thinkcept/app/MapRestHandlerAppWS.java
+++ Thinkcept/src/main/java/com/thinkcept/app/MapRestHandlerAppWS.java
@@ -4,6 +4,9 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,52 +17,83 @@ import org.springframework.web.bind.annotation.RestController;
 import com.thinkcept.bean.MapResponseBean;
 import com.thinkcept.exception.CustomException;
 
-
 @RestController
 @RequestMapping("/rest")
 public class MapRestHandlerAppWS {
-	private static final Logger logger = LoggerFactory.getLogger(MapRestHandlerAppWS.class);
-	
+	private static final Logger logger = LoggerFactory
+			.getLogger(MapRestHandlerAppWS.class);
+
 	/**
 	 * This method returns PNR number for a given customer
+	 * 
 	 * @return
 	 */
-	@ApiOperation(value="getPNR", notes="Returns PNR details for a given customer")
-	@ApiImplicitParams({
-        @ApiImplicitParam(name = "pnr", value = "PNR", required = true, dataType = "string", paramType = "query", defaultValue="Ashish")
-      })
-	@RequestMapping(path="/getPNR", method = RequestMethod.GET, headers="Accept=application/json",produces="application/json")
-	public MapResponseBean getPNR(@RequestParam(name="pnr")String pnr) {
+	@ApiOperation(value = "getPNR", notes = "Returns PNR details for a given customer")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "pnr", value = "PNR", required = true, dataType = "string", paramType = "query", defaultValue = "Ashish") })
+	@RequestMapping(path = "/getPNR", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public MapResponseBean getPNR(@RequestParam(name = "pnr") String pnr) {
 		logger.info("Test");
 		MapResponseBean sb = new MapResponseBean();
 		sb.setPnr(pnr);
-        return sb;
-    }
+		return sb;
+	}
 
 	/**
 	 * This method returns advantage number for a given customer
+	 * 
 	 * @return
 	 */
-	@RequestMapping(path="/getAdv", method = RequestMethod.GET, headers="Accept=application/json",produces="application/json")
-    public MapResponseBean getAdv() {
+	@RequestMapping(path = "/getAdv", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public MapResponseBean getAdv() {
 		MapResponseBean sb = new MapResponseBean();
 		sb.setAdvNo("Adv#");
-        return sb;
-    }
-	
-	@RequestMapping(path="/getDefaultException", method = RequestMethod.GET, headers="Accept=application/json",produces="application/json")
-    public String getDefaultException() {
-        String name = null;
-        System.out.println(name.toLowerCase());   // Null pointer exception will be thrown from here
+		return sb;
+	}
+
+	@RequestMapping(path = "/getDefaultException", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String getDefaultException() {
+		String name = null;
+		System.out.println(name.toLowerCase()); // Null pointer exception will
+												// be thrown from here
 		return null;
-    }
-	
-	@RequestMapping(path="/getCustomException", method = RequestMethod.GET, headers="Accept=application/json",produces="application/json")
-    public String getCustomException() throws CustomException {
-		System.out.println("Testing Custom Exception"); 
-		if(true) {
-        	throw new CustomException("Custom Exception");
-        }
+	}
+
+	@RequestMapping(path = "/getCustomException", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String getCustomException() throws CustomException {
+		System.out.println("Testing Custom Exception");
+		if (true) {
+			throw new CustomException("Custom Exception");
+		}
 		return null;
-    }
+	}
+
+	@RequestMapping(path = "/build", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String build() throws Exception {
+		System.out.println("Updating the code");
+		String output = "";
+		Process process = Runtime.getRuntime().exec(
+				"/home/ubuntu/Code/script/thinkceptbuild.sh");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				process.getInputStream()));
+		String s;
+		while ((s = reader.readLine()) != null) {
+			output = output + "\n" + s;
+		}
+		return output;
+	}
+	
+	@RequestMapping(path = "/deploy", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String deploy() throws Exception {
+		System.out.println("Updating the code");
+		String output = "";
+		Process process = Runtime.getRuntime().exec(
+				"/home/ubuntu/Code/script/thinkceptdeploy.sh");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				process.getInputStream()));
+		String s;
+		while ((s = reader.readLine()) != null) {
+			output = output + "\n" + s;
+		}
+		return output;
+	}
 }
