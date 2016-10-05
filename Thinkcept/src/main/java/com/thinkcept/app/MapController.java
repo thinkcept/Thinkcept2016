7a7b9f8389eda7e253cd7e9efa8ca19564e19885
thinkcept <ashismo@gmail.com>
2016-10-05 14:49:33 +0530

    Changes for travelerMap

--- Thinkcept/src/main/java/com/thinkcept/app/MapController.java
+++ Thinkcept/src/main/java/com/thinkcept/app/MapController.java
@@ -14,18 +14,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/mvc")
 public class MapController {
-	@RequestMapping(value="/static", method = RequestMethod.GET)
-	public String getStaticpage() {
-		return "redirect:/resources/test.html";
-	}
-	
-	@ApiOperation(value="mapRequest", notes="This service takes latitude and longitude as input and returns a page")
-	@ApiImplicitParams({
-        @ApiImplicitParam(name = "mapRequest", value = "Map Request handler", required = true, dataType = "string", paramType = "longitude", defaultValue="0")
-      })
-	@RequestMapping(value="/mapRequest", method = RequestMethod.GET)
-	public String getStudentName(@RequestParam String latitude,@RequestParam String longitude, ModelMap model) {
-		model.addAttribute("LatitudeLongitude", latitude + " " + longitude);
-		return "list";
-	}
+    @RequestMapping(value = "/static", method = RequestMethod.GET)
+    public String getStaticpage() {
+	return "redirect:/resources/test.html";
+    }
+
+    @RequestMapping(value = "/travelerMap", method = RequestMethod.GET)
+    public String travelerMap() {
+	return "redirect:/resources/travelerMap.html";
+    }
+
+    @RequestMapping(value = "/travelerMap.htm", method = RequestMethod.GET)
+    public String dynamicTravelerMap(@RequestParam double latitude, @RequestParam double longitude,
+	    @RequestParam String interest, ModelMap model) {
+	model.addAttribute("latitude", latitude);
+	model.addAttribute("longitude", longitude);
+	model.addAttribute("interest", interest);
+	return "travelerMap";
+    }
+
+    @ApiOperation(value = "mapRequest", notes = "This service takes latitude and longitude as input and returns a page")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "mapRequest", value = "Map Request handler", required = true, dataType = "string", paramType = "longitude", defaultValue = "0") })
+    @RequestMapping(value = "/mapRequest", method = RequestMethod.GET)
+    public String getStudentName(@RequestParam String latitude, @RequestParam String longitude, ModelMap model) {
+	model.addAttribute("LatitudeLongitude", latitude + " " + longitude);
+	return "list";
+    }
 }
