bb322382d844fbf735a41e04157999bda33b91a1
thinkcept <ashismo@gmail.com>
2016-10-15 02:55:18 +0530

    Update MapRestHandlerAppWS.java

--- Thinkcept/src/main/java/com/thinkcept/app/MapRestHandlerAppWS.java
+++ Thinkcept/src/main/java/com/thinkcept/app/MapRestHandlerAppWS.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.thinkcept.bean.MapResponseBean;
 import com.thinkcept.exception.CustomException;
+import com.thinkcept.util.RestServiceUtil;
 
 @RestController
 @RequestMapping("/rest")
@@ -36,6 +37,48 @@ public class MapRestHandlerAppWS {
 		MapResponseBean sb = new MapResponseBean();
 		sb.setPnr(pnr);
 		return sb;
+	}
+	
+	/**
+	 * This service store the registration token in server.
+	 * 
+	 * @return
+	 * @throws Exception 
+	 */
+	@ApiOperation(value = "storeToken", notes = "Store the registration token in server")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "user", value = "USER", required = true, dataType = "string", paramType = "query", defaultValue = "prabhat"),
+		@ApiImplicitParam(name = "token", value = "TOKEN", required = true, dataType = "string", paramType = "query", defaultValue = "123")})
+	@RequestMapping(path = "/storeToken", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String storeRegistrationToken(@RequestParam(name = "user") String user, @RequestParam(name = "token") String token) throws Exception {
+		RestServiceUtil util = new RestServiceUtil();
+		logger.debug("user:"+user+" token:"+token);
+		if(null != user && null != token) {
+			util.storeIntoFile(user+"."+RestServiceUtil.FIREBASE_TOKEN, token);
+		}
+		return "success";
+	}
+	
+	
+	/**
+	 * This service send push notification to mobile.
+	 * 
+	 * @return
+	 * @throws Exception 
+	 */
+	@ApiOperation(value = "sendNotification", notes = "Send push notification to mobile")
+	@ApiImplicitParams({ @ApiImplicitParam(name = "user", value = "USER", required = true, dataType = "string", paramType = "query", defaultValue = "prabhat"),
+		@ApiImplicitParam(name = "event", value = "EVENT", required = true, dataType = "string", paramType = "query", defaultValue = "0"),
+		@ApiImplicitParam(name = "latitude", value = "LATITUDE", required = true, dataType = "string", paramType = "query", defaultValue = "0"),
+		@ApiImplicitParam(name = "longitude", value = "LONGITUDE", required = true, dataType = "string", paramType = "query", defaultValue = "1")})
+	@RequestMapping(path = "/sendNotification", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public String sendPushNotification(@RequestParam(name = "user") String user, @RequestParam(name = "event") String event,
+			@RequestParam(name = "latitude") String latitude, @RequestParam(name = "longitude") String longitude) throws Exception {
+		RestServiceUtil util = new RestServiceUtil();
+		logger.debug("user:"+user+" event:"+event+" latitude:"+latitude+" longitude:"+longitude);
+		if(null != user && null != event && null != latitude && null != longitude) {
+			util.sendPushNotification(user, Integer.parseInt(event), latitude, longitude);
+		}
+		return "success";
 	}
 
 	/**
